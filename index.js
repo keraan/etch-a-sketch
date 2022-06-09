54e5397184c1a3abc5db858da650a33b95c62429
keraan <kierenhuynh88@gmail.com>
2022-06-10 09:09:20 +1000

    grid setup

---
+++ index.js
@@ -0,0 +1,18 @@
+let width = 50
+let height = 50
+const container = document.getElementById("container")
+
+
+
+
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      cell.innerText = (c + 1); // will delete later just for testing
+      container.appendChild(cell).className = "grid-item";
+    };
+  };
+  
+  makeRows(width, height);
