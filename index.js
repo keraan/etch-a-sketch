00451a29c40c08ae528dfd725dfed521b81fc84b
keraan <kierenhuynh88@gmail.com>
2022-06-10 10:20:39 +1000

    grid color working

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let width = 50
 let height = 50
 const container = document.getElementById("container")
+const cellEl = document.querySelector('.grid-item')
+const resetBtn = document.getElementById("reset-btn")
+const changeGridSizeBtn = document.getElementById("change-grid-size-btn")
+
 
 
 
@@ -9,10 +13,31 @@ function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      cell.innerText = (c + 1); // will delete later just for testing
+      const cell = document.createElement("div");
       container.appendChild(cell).className = "grid-item";
     };
   };
   
   makeRows(width, height);
+
+//container.addEventListener("mouseover", function(event) {
+//    if(event.target.id === event.currentTarget) {
+//        return;
+//    }
+//    event.target.style.backgroundColor = 'blue'
+//})
+
+function resetGrid() {
+    cellEl.style.backgroundColor = 'white';
+    console.log('clicked')
+}
+
+resetBtn.addEventListener("click", resetGrid)
+
+const handleInnerTargets = function(event){
+    if(event.target !== event.currentTarget){
+      event.target.style.backgroundColor='blue';
+    }
+  }
+  
+document.querySelector("#container").addEventListener("mouseover", handleInnerTargets);
